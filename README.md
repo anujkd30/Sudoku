39bf54f1bc5e379ff8426fd8bf4a991db16d7edb
anujkd30 <anujkd30@gmail.com>
2018-03-30 15:17:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sudoku
+
+Sudoku is one of the most famous puzzle game ever. With this Android app, you can play three different Sudoku levels:
+
+* Easy
+* Medium
+* Hard 
+
+This app generates a random Sudoku, that mean: countless time spent playing this game! However, you only have five attempts to solve
+the board game.
+
+The game incudes two keyboard modes:
+
+* **PEN**. When you know the definitive number in a cell, select pen mode to print it.
+* **PENCIL**. Use pencil mode to add notes in each cell. This mode will be useful to deduce the correct cell number.
+
+## **Screenshot**
+![Splash](https://github.com/pemCristina/Sudoku/blob/master/SudokuScreenshotSplash.png)
+![Game2](https://github.com/pemCristina/Sudoku/blob/master/SudokuScreenshotGame2.png)
+![Game1](https://github.com/pemCristina/Sudoku/blob/master/SudokuScreenshotGame1.png)
+
+
+## **Authors**
+
+Cristina Pérez - Initial work - [pemCristina](https://github.com/pemCristina)
---
+++ app/src/main/java/com/android/ejemplos/sudoku/model/Sudoku.java
@@ -0,0 +1,72 @@
+package com.android.ejemplos.sudoku.model;
+
+import android.content.Context;
+import android.os.SystemClock;
+
+import com.android.ejemplos.sudoku.R;
+import com.android.ejemplos.sudoku.activities.GameActivity;
+import com.android.ejemplos.sudoku.fragments.BoardGameFragment;
+import com.android.ejemplos.sudoku.fragments.KeyboardFragment;
+import com.android.ejemplos.sudoku.fragments.LifeFragment;
+import com.android.ejemplos.sudoku.util.AlertDialog;
+
+public class Sudoku {
+    private int lifeCounter = 4;
+    private static String[][] boardGame = {{"8", "2", "7", "1", "5", "4", "3", "9", "6"},
+                                           {"9", "6", "5", "3", "2", "7", "1", "4", "8"},
+                                           {"3", "4", "1", "6", "8", "9", "7", "5", "2"},
+                                           {"5", "9", "3", "4", "6", "8", "2", "7", "1"},
+                                           {"4", "7", "2", "5", "1", "3", "6", "8", "9"},
+                                           {"6", "1", "8", "9", "7", "2", "4", "3", "5"},
+                                           {"7", "8", "6", "2", "3", "5", "9", "1", "4"},
+                                           {"1", "5", "4", "7", "9", "6", "8", "2", "3"},
+                                           {"2", "3", "9", "8", "4", "1", "5", "6", "7"}};
+
+    public void resetGame(Context context, int numberOfCells, String level) {
+        BoardGameFragment.generateBoardGame(numberOfCells);
+        LifeFragment.restartIcons(context);
+        lifeCounter = 4;
+        GameActivity.setTextLevel(level);
+        GameActivity.chronometer.setBase(SystemClock.elapsedRealtime());
+        GameActivity.chronometer.start();
+        GameActivity.penPencilButton.setText(R.string.activity_board_game_pen_text);
+        GameActivity.penPencilButton.setEnabled(true);
+        KeyboardFragment.resetKeyboard();
+        KeyboardFragment.setEnabledKeyboard(true);
+    }
+
+    public void winGame(Context context) {
+        LifeFragment.restartIcons(context);
+        lifeCounter = 4;
+        AlertDialog.winner(context);
+        GameActivity.chronometer.stop();
+        GameActivity.penPencilButton.setText(R.string.activity_board_game_pen_text);
+        GameActivity.penPencilButton.setEnabled(false);
+        KeyboardFragment.resetKeyboard();
+        KeyboardFragment.setEnabledKeyboard(false);
+    }
+
+    public void loseGame(Context context) {
+        AlertDialog.gameOver(context);
+        BoardGameFragment.setPaintedCellOptionToFalseInBoardBame(BoardGameFragment.getArrayCell());
+        GameActivity.chronometer.stop();
+        GameActivity.penPencilButton.setText(R.string.activity_board_game_pen_text);
+        GameActivity.penPencilButton.setEnabled(false);
+        KeyboardFragment.resetKeyboard();
+        KeyboardFragment.setEnabledKeyboard(false);
+        LifeFragment.restartIcons(context);
+        lifeCounter = 4;
+    }
+
+    public int getLifeCounter() {
+        return lifeCounter;
+    }
+
+    public void setLifeCounter(int life_counter) {
+        this.lifeCounter = life_counter;
+    }
+
+    public static String[][] getBoardGame() {
+        return boardGame;
+    }
+}
